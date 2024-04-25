373064158928f54031df59ae13c44ea824c683a9
cmpsgabi <cmpsgabi@gmail.com>
2024-04-25 15:33:47 -0300

    login/signup update
    - cada usuário salvo separadamente
    - login funcionando

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
-document.querySelector("#entrar").addEventListener("click", function (){
-
-    user = document.getElementById("login").value;
+document.querySelector("#entrar").addEventListener("click", function (e){
+    e.preventDefault();
+    login = document.getElementById("login").value;
     password = document.getElementById("password").value; 
 
-    if(user === "" || password === ""){
+    if(login === "" || password === ""){
         alert("Campos vazios!");
     }else{
-        
-        if(user === localStorage.getItem("username") && password === localStorage.getItem("password"))
-        {
-            //alert("Credenciais corretas");
-            location.replace("C:\\Users\\cmpsg\\Documentos\\UTFPR\\Periodo9\\Desenvolvimento Web\\Web-Cliente\\index.html");
-        }
-        else
-        {
-            alert("Login ou senha incorretos!!");
+        for (let i = 0; i < localStorage.length; i++) {
+            if(localStorage.key(i) === login){
+                user = JSON.parse(localStorage.getItem(localStorage.key(i)));
+
+                if(password === user.password)
+                {
+                    //alert("Credenciais corretas");
+                    location.replace("C:/Users/cmpsg/Documentos/UTFPR/Periodo9/DesenvolvimentoWeb/Web-Cliente/index.html");
+                }
+                else
+                {
+                    alert("Login ou senha incorretos!!");
+                }
+            }
         }
     }
 })
